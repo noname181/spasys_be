2c35ae036d78b515418607fbdcbc5cda360c11e2
BinhTV <tuongvanbinh1998@gmail.com>
2022-07-06 20:34:19 +0700

    commit task #4

--- app/Http/Controllers/API/SendMailController.php
+++ app/Http/Controllers/API/SendMailController.php
@@ -15,7 +15,7 @@ class SendMailController extends Controller
     public function sendEmailOtp(Request $request)
     {
         if (empty($request->mb_no)) {
-            return response()->json(['status' => 422, 'message' => CommonFunc::renderMessage(Messages::MSG_0015, ['mb_no'])]);
+            return response()->json(['message' => CommonFunc::renderMessage(Messages::MSG_0015, ['mb_no'])], 400);
         }
         
         $mb_otp = rand(1000, 9999);
@@ -32,33 +32,33 @@ class SendMailController extends Controller
 
             Mail::to($member->mb_email)->send(new sendEmail($mail_details));
 
-            return response()->json(['status' => 200, 'message' => Messages::MSG_0007]);
+            return response()->json(['message' => Messages::MSG_0007], 200);
         } else {
-            return response()->json(['status' => 400, 'message' => Messages::MSG_0013]);
+            return response()->json(['message' => Messages::MSG_0013], 400);
         }
     }
 
     public function validateOtp(Request $request)
     {
         if (empty($request->mb_no)) {
-            return response()->json(['status' => 422, 'message' => CommonFunc::renderMessage(Messages::MSG_0015, ['mb_no'])]);
+            return response()->json(['message' => CommonFunc::renderMessage(Messages::MSG_0015, ['mb_no'])], 400);
         }
         if (empty($request->mb_otp)) {
-            return response()->json(['status' => 422, 'message' => CommonFunc::renderMessage(Messages::MSG_0015, ['mb_otp'])]);
+            return response()->json(['message' => CommonFunc::renderMessage(Messages::MSG_0015, ['mb_otp'])], 400);
         }
         $member = Member::where('mb_no', '=', $request->mb_no)->first();
 
         if (!empty($member)) {
 
             if ($request->mb_otp !== $member->mb_otp) {
-                return response()->json(['status' => 400, 'message' => Messages::MSG_0014]);
+                return response()->json(['message' => Messages::MSG_0014], 400);
             }
 
             Member::where('mb_no', '=', $member->mb_no)->update(['mb_otp' => null]);
 
-            return response()->json(['status' => 200, 'message' => Messages::MSG_0007]);
+            return response()->json(['message' => Messages::MSG_0007], 200);
         } else {
-            return response()->json(['status' => 400, 'message' => Messages::MSG_0013]);
+            return response()->json(['message' => Messages::MSG_0013], 400);
         }
     }
 
@@ -66,11 +66,11 @@ class SendMailController extends Controller
     {
 
         if (empty($request->mb_email)) {
-            return response()->json(['status' => 422, 'message' => CommonFunc::renderMessage(Messages::MSG_0015, ['mb_email'])]);
+            return response()->json(['message' => CommonFunc::renderMessage(Messages::MSG_0015, ['mb_email'])], 400);
         }
 
         if (CommonFunc::isMail($request->mb_email) == false) {
-            return response()->json(['status' => 422, 'message' => CommonFunc::renderMessage(Messages::MSG_0016, ['mb_email'])]);
+            return response()->json(['message' => CommonFunc::renderMessage(Messages::MSG_0016, ['mb_email'])], 400);
         }
 
         $mb_otp = rand(1000, 9999);
@@ -86,9 +86,9 @@ class SendMailController extends Controller
             Member::where('mb_email', '=', $member->mb_email)->update(['mb_otp' => $mb_otp]);
             Mail::to($member->mb_email)->send(new sendEmail($mail_details));
 
-            return response()->json(['status' => 200, 'message' => Messages::MSG_0007]);
+            return response()->json(['message' => Messages::MSG_0007], 200);
         } else {
-            return response()->json(['status' => 400, 'message' => Messages::MSG_0012]);
+            return response()->json(['message' => Messages::MSG_0012], 400);
         }
     }
 }
