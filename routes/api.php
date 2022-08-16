bd28b0ed956a15d043b4b6141f9739a3a1c852f1
ndqtuan95 <106954421+ndqtuan95@users.noreply.github.com>
2022-08-16 16:24:53 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -112,7 +112,7 @@ Route::middleware('auth')->group(function () {
 
     Route::get('/rgd', [\App\Http\Controllers\ReceivingGoodsDelivery\ReceivingGoodsDeliveryController::class,'__invoke']);
     Route::post('/get_rgd', [\App\Http\Controllers\ReceivingGoodsDelivery\ReceivingGoodsDeliveryController::class,'getReceivingGoodsDelivery']);
-    Route::get('/rgd', [\App\Http\Controllers\ReceivingGoodsDelivery\ReceivingGoodsDeliveryController::class,'create']);
+    Route::post('/rgd', [\App\Http\Controllers\ReceivingGoodsDelivery\ReceivingGoodsDeliveryController::class,'create']);
 
     Route::prefix('adjustment_group')->name('adjustment_group.')->group(function () {
         Route::post('/create_or_update/{co_no}', [App\Http\Controllers\Adjustment\AdjustmentGroupController::class, 'create'])->name('register_adjustment_group');
