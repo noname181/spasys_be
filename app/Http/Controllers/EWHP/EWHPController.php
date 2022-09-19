a8644e277cb13b0abaeca043a823118d18d738d3
Hoài Thương <41870695+noname181@users.noreply.github.com>
2022-09-19 09:14:38 +0700

    edit

---
+++ app/Http/Controllers/EWHP/EWHPController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\ReceivingGoodsDelivery;
+
+use DateTime;
+use App\Models\Warehousing;
+use App\Models\Member;
+use App\Models\WarehousingRequest;
+use App\Models\ReceivingGoodsDelivery;
+use App\Models\WarehousingItem;
+use App\Utils\Messages;
+use App\Utils\CommonFunc;
+use Illuminate\Http\Request;
+use PhpParser\Node\Stmt\TryCatch;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Filesystem\Filesystem;
+use App\Models\File;
+use App\Models\Item;
+use App\Http\Requests\ReceivingGoodsDelivery\ReceivingGoodsDeliveryRequest;
+use App\Http\Requests\ReceivingGoodsDelivery\ReceivingGoodsDeliveryCreateRequest;
+use App\Http\Requests\ReceivingGoodsDelivery\ReceivingGoodsDeliveryCreateMobileRequest;
+use App\Http\Requests\ReceivingGoodsDelivery\ReceivingGoodsDeliveryFileRequest;
+
+class ReceivingGoodsDeliveryController extends Controller
+{
+    /**
+     * Fetch data
+     * @param  \App\Http\Requests\ReceivingGoodsDelivery\ReceivingGoodsDeliveryRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function import_schedule(ReceivingGoodsDeliveryRequest $request)
+    {
+        try {
+
+            return response()->json([
+                'msg' => 'ok'
+            ]);
+        } catch (\Exception $e) {
+            Log::error($e);
+            return response()->json(['message' => Messages::MSG_0018], 500);
+        }
+    }
+
+    public function import(ReceivingGoodsDeliveryRequest $request)
+    {
+        try {
+
+            return response()->json([
+                'msg' => 'ok'
+            ]);
+        } catch (\Exception $e) {
+            Log::error($e);
+            return response()->json(['message' => Messages::MSG_0018], 500);
+        }
+    }
+
+    public function export(ReceivingGoodsDeliveryRequest $request)
+    {
+        try {
+
+            return response()->json([
+                'msg' => 'ok'
+            ]);
+        } catch (\Exception $e) {
+            Log::error($e);
+            return response()->json(['message' => Messages::MSG_0018], 500);
+        }
+    }
+
+}
