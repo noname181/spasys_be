5351eda6e60ee1f5802ae9b52dd00a8759779a12
huykashukic <46738539+huykashukic@users.noreply.github.com>
2022-10-05 14:34:12 +0700

    Update ImportScheduleController.php

--- app/Http/Controllers/ImportSchedule/ImportScheduleController.php
+++ app/Http/Controllers/ImportSchedule/ImportScheduleController.php
@@ -108,13 +108,38 @@ class ImportScheduleController extends Controller
     public function getImportAPI(ImportScheduleSearchRequest $request)
     {
         try {
-            DB::enableQueryLog();
+           
             $validated = $request->validated();
 
             // If per_page is null set default data = 15
             $per_page = isset($validated['per_page']) ? $validated['per_page'] : 15;
             // If page is null set default data = 1
             $page = isset($validated['page']) ? $validated['page'] : 1;
+
+            $sql = DB::select(DB::raw("select * from 
+            (select tie_logistic_manage_number from t_import_expected where tie_is_date >= '2022-01-04' and tie_is_date <= '2022-10-04' group by tie_logistic_manage_number) as aaa 
+            left outer join 
+            (SELECT te_logistic_manage_number,te_carry_out_number FROM t_export group by te_logistic_manage_number, te_carry_out_number ) as bbb 
+            on 
+            aaa.tie_logistic_manage_number = bbb.te_logistic_manage_number"));
+
+           
+            DB::statement("set session sql_mode='STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION'");
+            DB::enableQueryLog();
+            $byMobile   =   ImportExpected::whereNull('tie_logistic_manage_number');
+
+            $sql1 = ImportExpected::with(['import','company'])->groupBy('t_import_expected.tie_logistic_manage_number')->leftjoin('t_export', 't_import_expected.tie_logistic_manage_number', '=', 't_export.te_logistic_manage_number')
+            ->select(['t_import_expected.*','t_export.te_logistic_manage_number','t_export.te_carry_out_number'])
+            ->where('tie_is_date','>=','2022-01-04')->where('tie_is_date','<=','2022-10-04')
+            ->groupBy('t_export.te_logistic_manage_number','t_export.te_carry_out_number')->paginate($per_page, ['*'], 'page', $page);
+
+            //return DB::getQueryLog();
+
+            DB::statement("set session sql_mode='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION'");
+            
+            
+            //$sql2 = DB::table('t_export')->select('te_logistic_manage_number','te_carry_out_number')->groupBy('te_logistic_manage_number','te_carry_out_number')->get();
+
             $import_schedule = ImportExpected::with(['import','company'])->orderBy('tie_no', 'DESC');
            
             if (isset($validated['from_date'])) {
@@ -160,7 +185,7 @@ class ImportScheduleController extends Controller
 
             $import_schedule = $import_schedule->paginate($per_page, ['*'], 'page', $page);
 
-            return response()->json($import_schedule);
+            return response()->json($sql1);
         } catch (\Exception $e) {
             Log::error($e);
             return $e;
