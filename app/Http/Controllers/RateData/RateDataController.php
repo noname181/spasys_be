4cfe9720beb9e34571e3bdad3af2f49d605e3147
Hoài Thương <41870695+noname181@users.noreply.github.com>
2022-10-11 13:21:46 +0700

    Revert "Update RateDataController.php"
    This reverts commit b59c367b128ec58d4cae93e94b34dfe4299dd89b.

--- app/Http/Controllers/RateData/RateDataController.php
+++ app/Http/Controllers/RateData/RateDataController.php
@@ -164,7 +164,7 @@ class RateDataController extends Controller
         } catch (\Exception $e) {
             DB::rollback();
             Log::error($e);
-            return $e;
+
             return response()->json(['message' => Messages::MSG_0001], 500);
         }
     }
