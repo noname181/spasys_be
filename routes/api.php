89432ceae1a2f19eaed7c58712df51f9f8e8c74a
HungDoan <hungdoan.it95@gmail.com>
2022-10-21 11:32:58 +0700

    distribution_add_casebill_issue

--- routes/api.php
+++ routes/api.php
@@ -390,6 +390,8 @@ Route::middleware('auth')->group(function () {
         Route::get('/download_full_fillment_final/{rgd_no}',[\App\Http\Controllers\RateData\RateDataController::class, 'download_full_fillment_final']);
         //distribution_final_monthbill_issue
         Route::post('/download_final_monthbill_issue',[\App\Http\Controllers\RateData\RateDataController::class, 'download_final_monthbill_issue']);
+        //distribution_add_casebill_issue
+        Route::get('/download_add_casebill_issue/{rgd_no}',[\App\Http\Controllers\RateData\RateDataController::class, 'download_add_casebill_issue']);
 
         //GET GENERAL DATA FOLLOW BILL TYPE
         Route::get('/general/{rgd_no}/{bill_type}', [\App\Http\Controllers\RateData\RateDataController::class, 'get_rate_data_general'])->name('get_rate_data_general');
