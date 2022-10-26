f78ee862efb9a301b866db838d20832ceed7549d
oniiyachan <72971755+oniiyachan@users.noreply.github.com>
2022-10-26 10:19:28 +0700

    Update ImportController.php

--- app/Http/Controllers/Import/ImportController.php
+++ app/Http/Controllers/Import/ImportController.php
@@ -60,10 +60,17 @@ class ImportController extends Controller
        $per_page = isset($validated['per_page']) ? $validated['per_page'] : 15;
        // If page is null set default data = 1
        $page = isset($validated['page']) ? $validated['page'] : 1;
-       $warehousing_request = WarehousingRequest::with('mb_no')->orderBy('wr_no', 'DESC');
+       
+       $warehousing = Warehousing::where('w_no', '=', $validated['w_no'])->first();
+       $warehousing_request = WarehousingRequest::with(['mb_no','warehousing'])->orderBy('wr_no', 'DESC');
 
        $members = Member::where('mb_no', '!=', 0)->get();
 
+       if($warehousing){
+            $warehousing_request = $warehousing_request->where('w_no', '=', $validated['w_no'])->orwhere('w_no', '=', $warehousing->w_import_no);
+        }else{
+            $warehousing_request = $warehousing_request->where('w_no', '=', $validated['w_no']);
+        }
        $warehousing_request = $warehousing_request->paginate($per_page, ['*'], 'page', $page);
 
         //fetchItems
