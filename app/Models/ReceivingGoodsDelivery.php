073dcaba810fa76a3b0a59c3f03ba2e56cf0913c
HungDoan <hungdoan.it95@gmail.com>
2022-11-02 09:36:57 +0700

    update

--- app/Models/ReceivingGoodsDelivery.php
+++ app/Models/ReceivingGoodsDelivery.php
@@ -66,7 +66,7 @@ class ReceivingGoodsDelivery extends Model
 
     public function w_no()
     {
-        return $this->hasOne(Warehousing::class, 'w_no', 'w_no')->with(['co_no', 'warehousing_item','w_import_parent','warehousing_child', 'warehousing_request', 'import_expect','w_ew'])->withSum('warehousing_item_IW_spasys_confirm', 'wi_number');;
+        return $this->hasOne(Warehousing::class, 'w_no', 'w_no')->with(['co_no', 'warehousing_item','w_import_parent','warehousing_child', 'warehousing_request', 'import_expect','w_ew'])->withSum('warehousing_item_IW_spasys_confirm', 'wi_number');
     }
 
     public function warehousing()
