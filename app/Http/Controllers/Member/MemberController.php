c0ba0732b45974e657c639f9b9cdf38d176d342f
HungDoan <hungdoan.it95@gmail.com>
2022-11-05 09:33:21 +0700

    udpate

--- app/Http/Controllers/Member/MemberController.php
+++ app/Http/Controllers/Member/MemberController.php
@@ -79,7 +79,7 @@ class MemberController extends Controller
         try {
             $member = Member::where('mb_name', $request['mb_name'])
                 ->where('mb_email', $request['mb_email'])->first();
-
+            return $member;
             if (is_null($member)) {
                 return response()->json([
                     'message' => Messages::MSG_0020,
