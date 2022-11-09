60adf26f1e38caf05a65744aee5d186c1dfc1923
phunghuynh17 <65905034+phunghuynh17@users.noreply.github.com>
2022-11-09 08:49:19 -0800

    p

--- app/Http/Controllers/Warehousing/WarehousingController.php
+++ app/Http/Controllers/Warehousing/WarehousingController.php
@@ -3167,13 +3167,31 @@ class WarehousingController extends Controller
                     });
                 });
             } else if ($user->mb_type == 'shipper') {
-                $warehousing = ReceivingGoodsDelivery::with(['mb_no', 'w_no', 'rate_data_general'])->whereHas('w_no', function ($query) use ($user) {
+                $warehousing = ReceivingGoodsDelivery::with(['mb_no', 'w_no', 'rate_data_general','rate_meta_data' => function ($q) {
+
+                    $q->withCount([
+                        'rate_data as bonusQuantity' => function ($query) {
+
+                            $query->select(DB::raw('SUM(rd_data4)'))->where('rd_cate2','소계');
+                        },
+                    ]);
+                   
+                }])->whereHas('w_no', function ($query) use ($user) {
                     $query->whereHas('co_no', function ($q) use ($user) {
                         $q->where('co_no', $user->co_no);
                     });
                 });
             } else if ($user->mb_type == 'spasys') {
-                $warehousing = ReceivingGoodsDelivery::with(['mb_no', 'w_no', 'rate_data_general'])->whereHas('w_no', function ($query) use ($user) {
+                $warehousing = ReceivingGoodsDelivery::with(['mb_no', 'w_no', 'rate_data_general','rate_meta_data' => function ($q) {
+
+                    $q->withCount([
+                        'rate_data as bonusQuantity' => function ($query) {
+
+                            $query->select(DB::raw('SUM(rd_data4)'))->where('rd_cate2','소계');
+                        },
+                    ]);
+                   
+                }])->whereHas('w_no', function ($query) use ($user) {
                     $query->whereHas('co_no.co_parent.co_parent', function ($q) use ($user) {
                         $q->where('co_no', $user->co_no);
                     });
