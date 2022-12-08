25dfc6326af1ad8033c714bde4cb5f4c74074ed8
HungDoan <hungdoan.it95@gmail.com>
2022-12-08 14:58:04 +0700

    update

---
+++ app/Models/StockStatusBad.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class StockStatusBad extends Model
+{
+    use HasFactory;
+    
+    protected $table = "stock_status_bad";
+
+    protected $primaryKey = 'id';
+
+    public $timestamps = false;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'id',
+        'product_id',
+        'stock',
+        'status',
+        'item_no'
+    ];
+    public function item_status_bad()
+    {
+        return $this->belongsTo(Item::class, 'product_id', 'product_id');
+    }
+}
