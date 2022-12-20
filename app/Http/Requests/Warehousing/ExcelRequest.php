45dc925edd4446a4c48f6e109ffa75cff51c870e
huykashukic <46738539+huykashukic@users.noreply.github.com>
2022-12-20 17:00:35 +0700

    up

---
+++ app/Http/Requests/Warehousing/ExcelRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Requests\Item;
+
+use App\Http\Requests\BaseFormRequest;
+
+class ExcelRequest extends BaseFormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public static function rules()
+    {
+        return [
+            'A' => [
+                'required',
+                'max:255',
+            ],
+            'B' => [
+                'required',
+                'max:255',
+            ],
+            'C' => [
+                'required',
+                'max:255',
+            ]
+        ];
+    }
+
+    /**
+     * Get custom attributes for validator errors.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [];
+    }
+}
