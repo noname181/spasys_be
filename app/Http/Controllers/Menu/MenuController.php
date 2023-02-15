60050edb192678b91ca3047b823d636965548e76
phunghuynh17 <65905034+phunghuynh17@users.noreply.github.com>
2023-02-15 13:49:02 +0700

    p

--- app/Http/Controllers/Menu/MenuController.php
+++ app/Http/Controllers/Menu/MenuController.php
@@ -116,7 +116,18 @@ class MenuController extends Controller
                 ], 201);
             }
             }
-            if (isset($validated['menu_name'])) {
+            if (isset($validated['menu_name']) && isset($validated['menu_url'])) {
+                $service_no_array = explode(" ", $validated['menu_service_no_array']);
+                foreach($service_no_array as $row){
+                    $menu_check_name = Menu::where('menu_name',$validated['menu_name'])->first();
+                    if(isset($menu_check_name)){
+                        return response()->json([
+                            'error'=>'same_name'
+                        ], 201);
+                    }
+                }
+            }
+            if (isset($validated['menu_name']) && isset($validated['menu_url'])) {
                 $service_no_array = explode(" ", $validated['menu_service_no_array']);
                 foreach($service_no_array as $row){
                     $menu_check_name = Menu::where('menu_name',$validated['menu_name'])->where('service_no_array', 'like', '%' . $row . '%')->first();
