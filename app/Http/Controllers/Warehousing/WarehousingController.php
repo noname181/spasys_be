42ccd9710250d6268a91f89203b35eecc64a059b
phunghuynh17 <65905034+phunghuynh17@users.noreply.github.com>
2023-03-07 16:23:19 +0700

    p

--- app/Http/Controllers/Warehousing/WarehousingController.php
+++ app/Http/Controllers/Warehousing/WarehousingController.php
@@ -4157,9 +4157,10 @@ class WarehousingController extends Controller
                         $item->sum_price_total3 = isset($item->rate_data_general) ? $item->rate_data_general->rdg_sum7 : 0;
                     } else if($item->rate_data_general->rdg_sum4){
                         $item->sum_price_total3 = isset($item->rate_data_general) ? $item->rate_data_general->rdg_sum4 : 0;
-                    } else {
-                        $item->sum_price_total3 = 0;
                     }
+                    //  else {
+                    //     $item->sum_price_total3 = 0;
+                    // }
 
 
                     if ($i == $k) {
