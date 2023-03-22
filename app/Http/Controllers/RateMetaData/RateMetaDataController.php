152ad7df18fa0e0a978bf6c5889555bb71dcfb63
phunghuynh17 <65905034+phunghuynh17@users.noreply.github.com>
2023-03-22 13:14:25 +0700

    p

--- app/Http/Controllers/RateMetaData/RateMetaDataController.php
+++ app/Http/Controllers/RateMetaData/RateMetaDataController.php
@@ -145,7 +145,7 @@ class RateMetaDataController extends Controller
             }
             if(isset($validated['co_parent_name'])) {
                 $rmd->whereHas('company', function($rm) use ($validated){
-                    $rm->where('rm_owner_name', 'like', '%'.$validated['rm_owner_name'].'%');
+                    $rm->where('co_name', 'like', '%'.$validated['co_parent_name'].'%');
                 });
             }
 
@@ -168,7 +168,7 @@ class RateMetaDataController extends Controller
             return response()->json($rmd);
         } catch (\Exception $e) {
             Log::error($e);
-
+            return $e;
             return response()->json(['message' => Messages::MSG_0018], 500);
         }
     }
