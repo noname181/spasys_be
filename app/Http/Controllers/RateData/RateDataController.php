3d184799bd7bd60d8ab3fb52e4a5210e013332d9
Hoài Thương <41870695+noname181@users.noreply.github.com>
2023-05-08 13:36:05 +0700

    Update RateDataController.php

--- app/Http/Controllers/RateData/RateDataController.php
+++ app/Http/Controllers/RateData/RateDataController.php
@@ -2754,7 +2754,7 @@ class RateDataController extends Controller
             if($request->type == 'create_final'){
                 $est_rgd =  ReceivingGoodsDelivery::where('rgd_no', $final_rgd->rgd_parent_no)->first();
 
-                if($est_rgd->rgd_status6 != 'paid' && $est_rgd->rgd_calculate_deadline_yn == 'y' && !str_contains($est_rgd->rgd_bill_type, 'month')){
+                if($est_rgd->rgd_status6 != 'paid' && $est_rgd->service_korean_name == '보세화물' && $est_rgd->rgd_calculate_deadline_yn == 'y' && !str_contains($est_rgd->rgd_bill_type, 'month')){
                     ReceivingGoodsDelivery::where('rgd_settlement_number', $est_rgd->rgd_settlement_number)->update([
                         'rgd_status6' => 'paid',
                         'is_expect_payment' => 'y', //NOT REAL PAID
@@ -3594,7 +3594,7 @@ class RateDataController extends Controller
                         //UPDATE EST BILL WHEN ISSUE FINAL BILL
                         $est_rgd =  ReceivingGoodsDelivery::where('rgd_no', $final_rgd->rgd_parent_no)->first();
 
-                        if($est_rgd->rgd_status6 != 'paid' && $est_rgd->rgd_calculate_deadline_yn == 'y' && !str_contains($est_rgd->rgd_bill_type, 'month')){
+                        if($est_rgd->rgd_status6 != 'paid' && $est_rgd->service_korean_name == '보세화물' && $est_rgd->rgd_calculate_deadline_yn == 'y' && !str_contains($est_rgd->rgd_bill_type, 'month')){
 
                             ReceivingGoodsDelivery::where('rgd_settlement_number', $est_rgd->rgd_settlement_number)->update([
                                 'rgd_status6' => 'paid',
@@ -3839,7 +3839,7 @@ class RateDataController extends Controller
                     //UPDATE EST BILL WHEN ISSUE FINAL BILL
                     $est_rgd =  ReceivingGoodsDelivery::where('rgd_no', $final_rgd->rgd_parent_no)->first();
 
-                    if($est_rgd->rgd_status6 != 'paid' && $est_rgd->rgd_calculate_deadline_yn == 'y' && !str_contains($est_rgd->rgd_bill_type, 'month')){
+                    if($est_rgd->rgd_status6 != 'paid' && $est_rgd->service_korean_name == '보세화물' && $est_rgd->rgd_calculate_deadline_yn == 'y' && !str_contains($est_rgd->rgd_bill_type, 'month')){
 
                         ReceivingGoodsDelivery::where('rgd_settlement_number', $est_rgd->rgd_settlement_number)->update([
                             'rgd_status6' => 'paid',
@@ -4292,7 +4292,7 @@ class RateDataController extends Controller
                 //UPDATE EST BILL WHEN ISSUE FINAL BILL
                 $est_rgd =  ReceivingGoodsDelivery::where('rgd_no', $final_rgd->rgd_parent_no)->first();
 
-                if($est_rgd->rgd_status6 != 'paid' && $est_rgd->rgd_calculate_deadline_yn == 'y' && !str_contains($est_rgd->rgd_bill_type, 'month')){
+                if($est_rgd->rgd_status6 != 'paid' && $est_rgd->service_korean_name == '보세화물' && $est_rgd->rgd_calculate_deadline_yn == 'y' && !str_contains($est_rgd->rgd_bill_type, 'month')){
                     ReceivingGoodsDelivery::where('rgd_settlement_number', $est_rgd->rgd_settlement_number)->update([
                         'rgd_status6' => 'paid',
                         'is_expect_payment' => 'y', //NOT REAL PAID
