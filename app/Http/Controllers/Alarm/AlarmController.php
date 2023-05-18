82a98eabee1e53c03959dd9e012e9633f0aee796
huykashukic <46738539+huykashukic@users.noreply.github.com>
2023-05-18 17:10:36 +0700

    Update AlarmController.php

--- app/Http/Controllers/Alarm/AlarmController.php
+++ app/Http/Controllers/Alarm/AlarmController.php
@@ -626,6 +626,9 @@ class AlarmController extends Controller
                     })->orwhere(function($q) use ($user) {
                         $q->whereNotNull('receiver_no')->where('alarm_type','like','cargo_status3_EW')
                         ->where('receiver_no', $user->mb_no);
+                    })->orwhere(function($q) use ($user) {
+                        $q->whereNotNull('receiver_no')->where('alarm_type','like','cargo_TIE')
+                        ->where('receiver_no', $user->mb_no);
                     });
                 })->orderBy('alarm_no', 'DESC');
             }
@@ -675,6 +678,9 @@ class AlarmController extends Controller
                         ->where('receiver_no', $user->mb_no);
                     })->orwhere(function($q) use ($user) {
                         $q->whereNotNull('receiver_no')->where('alarm_type','like','cargo_status3_EW')
+                        ->where('receiver_no', $user->mb_no);
+                    })->orwhere(function($q) use ($user) {
+                        $q->whereNotNull('receiver_no')->where('alarm_type','like','cargo_TIE')
                         ->where('receiver_no', $user->mb_no);
                     });
                 })
