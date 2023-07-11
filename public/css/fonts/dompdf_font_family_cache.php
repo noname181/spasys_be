22c799e190af12bc9d6057c0c85070bda6057a27
Hoài Thương <41870695+noname181@users.noreply.github.com>
2023-07-11 13:42:39 +0700

    add

--- public/css/fonts/dompdf_font_family_cache.php
+++ public/css/fonts/dompdf_font_family_cache.php
@@ -1,16 +1,11 @@
 <?php 
 return  array (
-    "cwtexkai-zhonly" =>  array (
-        "normal" => "C:\\Users\\rians\\Documents\\GitHub\\spasys_be\\vendor\\dompdf\\dompdf\/lib\/fonts\/cwTeXKai-zhonly",
-        "bold" => "C:\\Users\\rians\\Documents\\GitHub\\spasys_be\\vendor\\dompdf\\dompdf\/lib\/fonts\/cwTeXKai-zhonly",
-        "italic" => "C:\\Users\\rians\\Documents\\GitHub\\spasys_be\\vendor\\dompdf\\dompdf\/lib\/fonts\/cwTeXKai-zhonly",
-        "bold_italic" => "C:\\Users\\rians\\Documents\\GitHub\\spasys_be\\vendor\\dompdf\\dompdf\/lib\/fonts\/cwTeXKai-zhonly"
-    ),
+
     "unbatang" => array (
-        "normal" => "C:\\Users\\rians\\Documents\\GitHub\\spasys_be\\vendor\\dompdf\\dompdf\/lib\/fonts\/UnBatang",
-        "bold" => "C:\\Users\\rians\\Documents\\GitHub\\spasys_be\\vendor\\dompdf\\dompdf\/lib\/fonts\/UnBatang",
-        "italic" => "C:\\Users\\rians\\Documents\\GitHub\\spasys_be\\vendor\\dompdf\\dompdf\/lib\/fonts\/UnBatang",
-        "bold_italic" => "C:\\Users\\rians\\Documents\\GitHub\\spasys_be\\vendor\\dompdf\\dompdf\/lib\/fonts\/UnBatang"
+        "normal" => "E:\\Spasys\\spasys\\laravel\\vendor\\dompdf\\dompdf\/lib\/fonts\/UnBatang",
+        "bold" => "E:\\Spasys\\spasys\\laravel\\vendor\\dompdf\\dompdf\/lib\/fonts\/UnBatang",
+        "italic" => "E:\\Spasys\\spasys\\laravel\\vendor\\dompdf\\dompdf\/lib\/fonts\/UnBatang",
+        "bold_italic" => "E:\\Spasys\\spasys\\laravel\\vendor\\dompdf\\dompdf\/lib\/fonts\/UnBatang"
     )
 );
  ?>
