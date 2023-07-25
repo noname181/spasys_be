ea55be48e20769f9f10278ac1973815abe6ae2fd
Hoài Thương <41870695+noname181@users.noreply.github.com>
2023-07-25 11:45:08 +0700

    Update CommonFunc.php

--- app/Utils/CommonFunc.php
+++ app/Utils/CommonFunc.php
@@ -62,11 +62,11 @@ class CommonFunc
     static function generate_rmd_number($id, $index)
     {
         if($index < 10){
-            $index = '000' + $index;
+            $index = '000' . $index;
         }else  if($index < 100){
-            $index = '00' + $index;
+            $index = '00' . $index;
         }else  if($index < 1000){
-            $index = '0' + $index;
+            $index = '0' . $index;
         }
 
         $string = date('Ymd') . '_' . $index;
