bde8d6aad056895a8f224bae01721b4914c86bd3
Hoài Thương <41870695+noname181@users.noreply.github.com>
2023-08-28 08:58:55 +0700

    Update WarehousingController.php

--- app/Http/Controllers/Warehousing/WarehousingController.php
+++ app/Http/Controllers/Warehousing/WarehousingController.php
@@ -7541,7 +7541,7 @@ class WarehousingController extends Controller
                     ]);
                 }
 
-                $rgd = ReceivingGoodsDelivery::with(['warehousing', 'rate_data_general'])->where('rgd_no', $request->rgd_no)->first();
+                $rgd = ReceivingGoodsDelivery::with(['warehousing', 'rate_data_general', 't_import_expected'])->where('rgd_no', $request->rgd_no)->first();
 
                 $api = $this->tax_invoice_api($rgd, $user, null, $tax_number, $request->rgds, null, $request['ag']['ag_email'] ? $request['ag']['ag_email'] : null);
 
@@ -9929,14 +9929,35 @@ class WarehousingController extends Controller
             $vat_price = $price['tid_vat'];
 
             if ($rgd['service_korean_name'] == '보세화물') {
-                $item_name = $rgd['t_import']['ti_h_bl'];
-            
+                if (str_contains($rgd['rgd_bill_type'], 'month')) {
+                    $count = ReceivingGoodsDelivery::where('rgd_settlement_number', $rgd['rgd_settlement_number'])->count();
+                    $item_name = $rgd['t_import']['ti_h_bl'] . ($count == 1 ? '' : ('외 ' . ($count - 1) . '건'));
+                } else {
+                    $item_name = $rgd['t_import']['ti_h_bl'];
+                }
+                $amount_price += ($rgd['rate_data_general']['rdg_supply_price7'] + $rgd['rate_data_general']['rdg_supply_price14']);
+                $total_price += ($rgd['rate_data_general']['rdg_sum7'] + $rgd['rate_data_general']['rdg_sum14']);
+                $vat_price += ($rgd['rate_data_general']['rdg_vat7'] + $rgd['rate_data_general']['rdg_vat14']);
             } else if ($rgd['service_korean_name'] == '수입풀필먼트') {
-                $item_name = $rgd['rgd_settlement_number'];
-              
+                if (str_contains($rgd['rgd_bill_type'], 'month')) {
+                    $count = ReceivingGoodsDelivery::where('rgd_settlement_number', $rgd['rgd_settlement_number'])->count();
+                    $item_name = $rgd['rgd_settlement_number'] . ($count == 1 ? '' : ('외 ' . ($count - 1) . '건'));
+                } else {
+                    $item_name = $rgd['rgd_settlement_number'];
+                }
+                $amount_price += $rgd['rate_data_general']['rdg_supply_price6'];
+                $total_price += $rgd['rate_data_general']['rdg_sum6'];
+                $vat_price += $rgd['rate_data_general']['rdg_vat6'];
             } else if ($rgd['service_korean_name'] == '유통가공') {
-                $item_name = $rgd['warehousing']['w_schedule_number2'];
-    
+                if (str_contains($rgd['rgd_bill_type'], 'month')) {
+                    $count = ReceivingGoodsDelivery::where('rgd_settlement_number', $rgd['rgd_settlement_number'])->count();
+                    $item_name = $rgd['warehousing']['w_schedule_number2'] . ($count == 1 ? '' : ('외 ' . ($count - 1) . '건'));
+                } else {
+                    $item_name = $rgd['warehousing']['w_schedule_number2'];
+                }
+                $amount_price += $rgd['rate_data_general']['rdg_supply_price4'];
+                $total_price += $rgd['rate_data_general']['rdg_sum4'];
+                $vat_price += $rgd['rate_data_general']['rdg_vat4'];
             }
 
             $items[] = array(
@@ -9955,17 +9976,49 @@ class WarehousingController extends Controller
             $vat_price = 0;
 
             if ($rgd['service_korean_name'] == '보세화물') {
-                $item_name = $rgd['rgd_settlement_number'];
+
                 $amount_price += ($rgd['rate_data_general']['rdg_supply_price7'] + $rgd['rate_data_general']['rdg_supply_price14']);
                 $total_price += ($rgd['rate_data_general']['rdg_sum7'] + $rgd['rate_data_general']['rdg_sum14']);
                 $vat_price += ($rgd['rate_data_general']['rdg_vat7'] + $rgd['rate_data_general']['rdg_vat14']);
             } else if ($rgd['service_korean_name'] == '수입풀필먼트') {
-                $item_name = $rgd['rgd_settlement_number'];
+
                 $amount_price += $rgd['rate_data_general']['rdg_supply_price6'];
                 $total_price += $rgd['rate_data_general']['rdg_sum6'];
                 $vat_price += $rgd['rate_data_general']['rdg_vat6'];
             } else if ($rgd['service_korean_name'] == '유통가공') {
-                $item_name = $rgd['rgd_settlement_number'];
+
+                $amount_price += $rgd['rate_data_general']['rdg_supply_price4'];
+                $total_price += $rgd['rate_data_general']['rdg_sum4'];
+                $vat_price += $rgd['rate_data_general']['rdg_vat4'];
+            }
+
+            if ($rgd['service_korean_name'] == '보세화물') {
+                if (str_contains($rgd['rgd_bill_type'], 'month')) {
+                    $count = ReceivingGoodsDelivery::where('rgd_settlement_number', $rgd['rgd_settlement_number'])->count();
+                    $item_name = $rgd['t_import']['ti_h_bl'] . ($count == 1 ? '' : ('외 ' . ($count - 1) . '건'));
+                } else {
+                    $item_name = $rgd['t_import']['ti_h_bl'];
+                }
+                $amount_price += ($rgd['rate_data_general']['rdg_supply_price7'] + $rgd['rate_data_general']['rdg_supply_price14']);
+                $total_price += ($rgd['rate_data_general']['rdg_sum7'] + $rgd['rate_data_general']['rdg_sum14']);
+                $vat_price += ($rgd['rate_data_general']['rdg_vat7'] + $rgd['rate_data_general']['rdg_vat14']);
+            } else if ($rgd['service_korean_name'] == '수입풀필먼트') {
+                if (str_contains($rgd['rgd_bill_type'], 'month')) {
+                    $count = ReceivingGoodsDelivery::where('rgd_settlement_number', $rgd['rgd_settlement_number'])->count();
+                    $item_name = $rgd['rgd_settlement_number'] . ($count == 1 ? '' : ('외 ' . ($count - 1) . '건'));
+                } else {
+                    $item_name = $rgd['rgd_settlement_number'];
+                }
+                $amount_price += $rgd['rate_data_general']['rdg_supply_price6'];
+                $total_price += $rgd['rate_data_general']['rdg_sum6'];
+                $vat_price += $rgd['rate_data_general']['rdg_vat6'];
+            } else if ($rgd['service_korean_name'] == '유통가공') {
+                if (str_contains($rgd['rgd_bill_type'], 'month')) {
+                    $count = ReceivingGoodsDelivery::where('rgd_settlement_number', $rgd['rgd_settlement_number'])->count();
+                    $item_name = $rgd['warehousing']['w_schedule_number2'] . ($count == 1 ? '' : ('외 ' . ($count - 1) . '건'));
+                } else {
+                    $item_name = $rgd['warehousing']['w_schedule_number2'];
+                }
                 $amount_price += $rgd['rate_data_general']['rdg_supply_price4'];
                 $total_price += $rgd['rate_data_general']['rdg_sum4'];
                 $vat_price += $rgd['rate_data_general']['rdg_vat4'];
@@ -10551,17 +10604,46 @@ class WarehousingController extends Controller
             $vat_price = $price['tid_vat'];
 
             if ($rgd['service_korean_name'] == '보세화물') {
-                $item_name = $rgd['t_import']['ti_h_bl'];
                 $amount_price += ($rgd['rate_data_general']['rdg_supply_price7'] + $rgd['rate_data_general']['rdg_supply_price14']);
                 $total_price += ($rgd['rate_data_general']['rdg_sum7'] + $rgd['rate_data_general']['rdg_sum14']);
                 $vat_price += ($rgd['rate_data_general']['rdg_vat7'] + $rgd['rate_data_general']['rdg_vat14']);
             } else if ($rgd['service_korean_name'] == '수입풀필먼트') {
-                $item_name = $rgd['rgd_settlement_number'];
                 $amount_price += $rgd['rate_data_general']['rdg_supply_price6'];
                 $total_price += $rgd['rate_data_general']['rdg_sum6'];
                 $vat_price += $rgd['rate_data_general']['rdg_vat6'];
             } else if ($rgd['service_korean_name'] == '유통가공') {
-                $item_name = $rgd['warehousing']['w_schedule_number2'];
+                $amount_price += $rgd['rate_data_general']['rdg_supply_price4'];
+                $total_price += $rgd['rate_data_general']['rdg_sum4'];
+                $vat_price += $rgd['rate_data_general']['rdg_vat4'];
+            }
+
+            if ($rgd['service_korean_name'] == '보세화물') {
+                if (str_contains($rgd['rgd_bill_type'], 'month')) {
+                    $count = ReceivingGoodsDelivery::where('rgd_settlement_number', $rgd['rgd_settlement_number'])->count();
+                    $item_name = $rgd['t_import']['ti_h_bl'] . ($count == 1 ? '' : ('외 ' . ($count - 1) . '건'));
+                } else {
+                    $item_name = $rgd['t_import']['ti_h_bl'];
+                }
+                $amount_price += ($rgd['rate_data_general']['rdg_supply_price7'] + $rgd['rate_data_general']['rdg_supply_price14']);
+                $total_price += ($rgd['rate_data_general']['rdg_sum7'] + $rgd['rate_data_general']['rdg_sum14']);
+                $vat_price += ($rgd['rate_data_general']['rdg_vat7'] + $rgd['rate_data_general']['rdg_vat14']);
+            } else if ($rgd['service_korean_name'] == '수입풀필먼트') {
+                if (str_contains($rgd['rgd_bill_type'], 'month')) {
+                    $count = ReceivingGoodsDelivery::where('rgd_settlement_number', $rgd['rgd_settlement_number'])->count();
+                    $item_name = $rgd['rgd_settlement_number'] . ($count == 1 ? '' : ('외 ' . ($count - 1) . '건'));
+                } else {
+                    $item_name = $rgd['rgd_settlement_number'];
+                }
+                $amount_price += $rgd['rate_data_general']['rdg_supply_price6'];
+                $total_price += $rgd['rate_data_general']['rdg_sum6'];
+                $vat_price += $rgd['rate_data_general']['rdg_vat6'];
+            } else if ($rgd['service_korean_name'] == '유통가공') {
+                if (str_contains($rgd['rgd_bill_type'], 'month')) {
+                    $count = ReceivingGoodsDelivery::where('rgd_settlement_number', $rgd['rgd_settlement_number'])->count();
+                    $item_name = $rgd['warehousing']['w_schedule_number2'] . ($count == 1 ? '' : ('외 ' . ($count - 1) . '건'));
+                } else {
+                    $item_name = $rgd['warehousing']['w_schedule_number2'];
+                }
                 $amount_price += $rgd['rate_data_general']['rdg_supply_price4'];
                 $total_price += $rgd['rate_data_general']['rdg_sum4'];
                 $vat_price += $rgd['rate_data_general']['rdg_vat4'];
@@ -10583,17 +10665,46 @@ class WarehousingController extends Controller
             $vat_price = 0;
 
             if ($rgd['service_korean_name'] == '보세화물') {
-                $item_name = $rgd['rgd_settlement_number'];
                 $amount_price += ($rgd['rate_data_general']['rdg_supply_price7'] + $rgd['rate_data_general']['rdg_supply_price14']);
                 $total_price += ($rgd['rate_data_general']['rdg_sum7'] + $rgd['rate_data_general']['rdg_sum14']);
                 $vat_price += ($rgd['rate_data_general']['rdg_vat7'] + $rgd['rate_data_general']['rdg_vat14']);
             } else if ($rgd['service_korean_name'] == '수입풀필먼트') {
-                $item_name = $rgd['rgd_settlement_number'];
                 $amount_price += $rgd['rate_data_general']['rdg_supply_price6'];
                 $total_price += $rgd['rate_data_general']['rdg_sum6'];
                 $vat_price += $rgd['rate_data_general']['rdg_vat6'];
             } else if ($rgd['service_korean_name'] == '유통가공') {
-                $item_name = $rgd['rgd_settlement_number'];
+                $amount_price += $rgd['rate_data_general']['rdg_supply_price4'];
+                $total_price += $rgd['rate_data_general']['rdg_sum4'];
+                $vat_price += $rgd['rate_data_general']['rdg_vat4'];
+            }
+
+            if ($rgd['service_korean_name'] == '보세화물') {
+                if (str_contains($rgd['rgd_bill_type'], 'month')) {
+                    $count = ReceivingGoodsDelivery::where('rgd_settlement_number', $rgd['rgd_settlement_number'])->count();
+                    $item_name = $rgd['t_import']['ti_h_bl'] . ($count == 1 ? '' : ('외 ' . ($count - 1) . '건'));
+                } else {
+                    $item_name = $rgd['t_import']['ti_h_bl'];
+                }
+                $amount_price += ($rgd['rate_data_general']['rdg_supply_price7'] + $rgd['rate_data_general']['rdg_supply_price14']);
+                $total_price += ($rgd['rate_data_general']['rdg_sum7'] + $rgd['rate_data_general']['rdg_sum14']);
+                $vat_price += ($rgd['rate_data_general']['rdg_vat7'] + $rgd['rate_data_general']['rdg_vat14']);
+            } else if ($rgd['service_korean_name'] == '수입풀필먼트') {
+                if (str_contains($rgd['rgd_bill_type'], 'month')) {
+                    $count = ReceivingGoodsDelivery::where('rgd_settlement_number', $rgd['rgd_settlement_number'])->count();
+                    $item_name = $rgd['rgd_settlement_number'] . ($count == 1 ? '' : ('외 ' . ($count - 1) . '건'));
+                } else {
+                    $item_name = $rgd['rgd_settlement_number'];
+                }
+                $amount_price += $rgd['rate_data_general']['rdg_supply_price6'];
+                $total_price += $rgd['rate_data_general']['rdg_sum6'];
+                $vat_price += $rgd['rate_data_general']['rdg_vat6'];
+            } else if ($rgd['service_korean_name'] == '유통가공') {
+                if (str_contains($rgd['rgd_bill_type'], 'month')) {
+                    $count = ReceivingGoodsDelivery::where('rgd_settlement_number', $rgd['rgd_settlement_number'])->count();
+                    $item_name = $rgd['warehousing']['w_schedule_number2'] . ($count == 1 ? '' : ('외 ' . ($count - 1) . '건'));
+                } else {
+                    $item_name = $rgd['warehousing']['w_schedule_number2'];
+                }
                 $amount_price += $rgd['rate_data_general']['rdg_supply_price4'];
                 $total_price += $rgd['rate_data_general']['rdg_sum4'];
                 $vat_price += $rgd['rate_data_general']['rdg_vat4'];
